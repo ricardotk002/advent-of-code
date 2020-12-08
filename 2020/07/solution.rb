aff8ff91861f0a96f1c6fd9efe31bd8144f04591
Ricardo Díaz <ricardotk002@gmail.com>
2020-12-07 22:02:17 -0500

    Solution for day 7

---
+++ 2020/07/solution.rb
@@ -0,0 +1,35 @@
+input = ARGF.read
+
+TREE = input.lines.reduce([]) do |xs, line|
+  line.strip!
+  root = line.match(/(\w+ \w+) bags contain/)[1]
+  leaves = line.scan(/(\d+) (\w+ \w+) bags?/)
+  xs << [root, leaves]
+  xs
+end
+
+def find_possible_containers(arr, *elems)
+  found = TREE.reduce([]) do |xs, x|
+    xs << x[0] if x[1].any? { |y| elems.include?(y[1]) }
+    xs
+  end
+
+  if found.empty?
+    arr
+  else
+    find_possible_containers(arr + found, *found)
+  end
+end
+
+def find_contained_elements(num, elem)
+  found = TREE.select { |x| x[0] == elem }.map { |y| y[1] }.flatten(1)
+
+  if found.empty?
+    num
+  else
+    num + num * found.map { |x| find_contained_elements(x[0].to_i, x[1]) }.sum
+  end
+end
+
+puts find_possible_containers([], "shiny gold").uniq.count
+puts find_contained_elements(1, "shiny gold") - 1
